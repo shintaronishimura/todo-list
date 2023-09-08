339caf895e808df4ffe8cccdc5c8f817f525890b
shintaronishimura <md.sakura0803@gmail.com>
2023-09-08 22:54:10 +0900

    タスクの追加機能を実装

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,1 +1,49 @@
 import { useState } from "react";
+
+const TodoList = () => {
+    const initialState = [
+        {
+            task: "Learn vue.js",
+            isCompleted: false,
+        },
+        {
+            task: "Learn React Hook",
+            isCompleted: false,
+        },
+        {
+            task: "Learn Getsby.js",
+            isCompleted: false,
+        }
+    ];
+    const [todos, setTodos] = useState(initialState);
+    const [task, setTask] = useState('');
+    const handleNewTask = (e) => {
+        setTask(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task === "") return;
+        console.log({ task, isCompleted: true });
+        console.log(...todos);
+        setTodos((todos) => [...todos, { task, isCompleted: false }]);
+        setTask('');
+    }
+
+    return (
+        <div>
+            <h1>TodoList</h1>
+            <form onSubmit={handleSubmit}>
+                Add Task : <input placeholder="Add New Task" onChange={handleNewTask} />
+                <button type="submit">Add</button>
+            </form>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo.task}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default TodoList
