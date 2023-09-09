45d88de0d528d976be07bf388bc4f5cb09bdae9d
shintaronishimura <md.sakura0803@gmail.com>
2023-09-09 13:12:35 +0900

    タスクの更新機能を追加

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,53 +1,96 @@
+import { HStack } from "@chakra-ui/react";
 import { useState } from "react";
 
 const TodoList = () => {
-    const [todos, setTodos] = useState([
-        {
-            task: "Learn vue.js",
-            isCompleted: false,
-        },
-        {
-            task: "Learn React Hook",
-            isCompleted: false,
-        },
-        {
-            task: "Learn Getsby.js",
-            isCompleted: false,
-        }
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+  const [finishied, setFinished] = useState([]);
+  const handleNewTask = (e) => {
+    setTask(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task === "") return;
+    setTodos((todos) => [
+      ...todos,
+      { id: Date.now(), task, isCompleted: false },
     ]);
-    const [task, setTask] = useState('');
-    const handleNewTask = (e) => {
-        setTask(e.target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (task === "") return;
-        console.log({ task, isCompleted: true });
-        console.log(...todos);
-        setTodos((todos) => [...todos, { task, isCompleted: false }]);
-        setTask('');
-    }
-    const handleDeleteTask = (index) => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
-    };
+    setTask("");
+  };
+  const handleDeleteTask = (id) => {
+    const newFinisheds = finishied.filter((task) => task.id !== id);
+    setFinished(newFinisheds);
 
-    return (
-        <div>
-            <h1>TodoList</h1>
-            <form onSubmit={handleSubmit}>
-                Add Task : <input placeholder="Add New Task" onChange={handleNewTask} />
-                <button type="submit">Add</button>
-            </form>
-            <ul>
-                {todos.map((todo, index) => (
-                    <li key={index}>
-                        {todo.task} <span style={{ cursor: 'pointer' }} onClick={() => handleDeleteTask(index)}> [×] </span>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-export default TodoList
+    const newTodos = todos.filter((task) => task.id !== id);
+    setTodos(newTodos);
+  };
+  const handleUpdatedTask = (id, isChecked) => {
+    const taskToMove = isChecked
+      ? finishied.find((task) => task.id === id)
+      : todos.find((task) => task.id === id);
+    if (isChecked) {
+      taskToMove.isCompleted = !isChecked;
+      setTodos((prevTodos) => [...prevTodos, taskToMove]);
+      setFinished((prevFinished) =>
+        prevFinished.filter((task) => task.id !== id)
+      );
+    } else {
+      taskToMove.isCompleted = !isChecked;
+      setFinished((prevFinished) => [...prevFinished, taskToMove]);
+      setTodos((prevTodos) => prevTodos.filter((task) => task.id !== id));
+    }
+  };
+
+  return (
+    <div>
+      <HStack>
+        <h1>やることリスト </h1>
+        <form onSubmit={handleSubmit}>
+          タスクを追加 :{" "}
+          <input placeholder="新しいタスクを追加" onChange={handleNewTask} />
+          <button type="submit">追加</button>
+        </form>
+      </HStack>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={() => handleUpdatedTask(todo.id, todo.isCompleted)}
+            />
+            {todo.task}{" "}
+            <span
+              style={{ cursor: "pointer" }}
+              onClick={() => handleDeleteTask(todo.id)}
+            >
+              {" "}
+              [×]{" "}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <h2>完了したタスク</h2>
+      <ul>
+        {finishied.map((finish, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={finish.isCompleted}
+              onChange={() => handleUpdatedTask(finish.id, finish.isCompleted)}
+            />
+            {finish.task}{" "}
+            <span
+              style={{ cursor: "pointer" }}
+              onClick={() => handleDeleteTask(finish.id)}
+            >
+              {" "}
+              [×]{" "}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TodoList;
